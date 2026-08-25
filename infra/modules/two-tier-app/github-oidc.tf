3b792e190b4e58ecf4b7d02b08e43e624a851ff6
Michele Formenti <michele.formenti@proton.me>
2026-08-25 10:43:59 +0200

    Harden GitHub Actions OIDC trust policy

--- infra/modules/two-tier-app/github-oidc.tf
+++ infra/modules/two-tier-app/github-oidc.tf
@@ -19,8 +19,9 @@ resource "aws_iam_role" "github_actions_ecr_role" {
       }
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:*"
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:environment:${var.env}"
         }
       }
     }]
